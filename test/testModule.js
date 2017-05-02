930d89fcca76e4906d3256e59ea4a05d2288d894
unknown <wati@vinlqa-aowati.india.corp.voxiva.net>
2017-05-02 16:37:13 +0530

    excel reader

--- test/testModule.js
+++ test/testModule.js
@@ -45,8 +45,8 @@ function get(){
 
 function google(callback){
     if(typeof XMLHttpRequest !== 'undefined'){
-        //axios.get('https://google.com').then(
-        axios.get('https://api.github.com/users/codeheaven-io').then(
+        axios.get('https://google.com').then(
+        //axios.get('https://api.github.com/users/codeheaven-io').then(
         //axios.get('http://oss.sheetjs.com/js-xlsx/test_files/formula_stress_test_ajax.xlsx').then(
         //axios.get('https://github.com/aowati100/repo1/raw/master/test/dummy.xlsx').then(            
             function(response){
@@ -89,27 +89,25 @@ function testPromise(){
 
 function readExcel(){ 
     //var fileName = 'http://oss.sheetjs.com/js-xlsx/test_files/formula_stress_test_ajax.xlsx';
-    //var fileName = 'file://D:\JS\demo3\test\dummy.xlsx';
     //var sheetName = 'Database';    
     var fileName = "./dummy.xlsx";
     var sheetName = 'SheetOne';    
     if(typeof XMLHttpRequest !== 'undefined'){        
         return new Promise(function(resolve, reject){
-            axios.get(fileName, {
-                headers: {
-                    'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-                }                
-            }).then(function(data){
+            var oReq;
+            oReq = new XMLHttpRequest();
+            oReq.open("GET", fileName, true);
+            oReq.responseType = "arraybuffer";
+            oReq.onload = function (e) {
+                var arraybuffer = oReq.response;
+                var data = new Uint8Array(arraybuffer);
                 var arr = new Array();
                 for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-                var bstr = arr.join("");
-                var workbook = XLSX.read(bstr, { type: "binary" });
-                var worksheet = workbook.Sheets[sheetName];
-                console.log("C");
-                var csv = XLSX.utils.sheet_to_csv(worksheet);
-                console.log(csv);
-                resolve();
-            });
+                var wb = XLSX.read(arr.join(""), { type: "binary" });
+                var csv = XLSX.utils.sheet_to_csv(wb.Sheets[sheetName]);
+                resolve(csv);
+            };
+            oReq.send();            
         });
     }
     else{
